cc015330840e8d639fa6d20c71a3ab2e52baa212
Joonhoo <mnc5@naver.com>
2022-01-08 15:30:31 +0900

    안드로이드 ViewModel 예시

---
+++ ViewModelDemo/app/src/main/java/com/kyonggi/viewmodeldemo/ui/main/MainFragment.kt
@@ -0,0 +1,46 @@
+package com.kyonggi.viewmodeldemo.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kyonggi.viewmodeldemo.R
+import com.kyonggi.viewmodeldemo.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+//        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.resultText.text = viewModel.getResult().toString()
+
+        binding.convertButton.setOnClickListener {
+            if (binding.dollarText.text.isNotEmpty()) {
+                viewModel.setAmount(binding.dollarText.text.toString())
+                binding.resultText.text = viewModel.getResult().toString()
+            } else {
+                binding.resultText.text = "No Value"
+            }
+        }
+    }
+
+}
